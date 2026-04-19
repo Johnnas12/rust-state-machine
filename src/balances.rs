713b9fb64e6353e7d7b08054a2e3cb9813b783a6
johnnas12 <yohanesmesfin3@gmail.com>
2026-04-19 16:21:39 +0300

    project structure on rust state machie

---
+++ src/balances.rs
@@ -0,0 +1,98 @@
+use std::collections::BTreeMap;
+pub struct Pallet {
+    balances: BTreeMap<String, u128>
+}
+
+impl Pallet {
+    pub fn new() -> Self {
+        Self {
+            balances : BTreeMap::new()
+        }
+    }
+
+    pub fn set_balance(&mut self, who: &String, amount: u128 ){
+        self.balances.insert(who.clone(), amount);
+    }
+
+    pub fn balance(&mut self, who: &String) -> u128 {
+        *self.balances.get(who).unwrap_or(&0)
+    }
+}
+
+impl Pallet {
+    pub fn transfer(&mut self, caller: String, to: String, amount: u128) -> Result<(), &'static str> {
+        let caller_balance = self.balance(&caller);
+        let to_balance = self.balance(&to);
+
+        let new_caller_balance = caller_balance.checked_sub(amount).ok_or("Insufficient Balance")?;
+        let new_to_balance = to_balance.checked_add(amount).ok_or("Overflow")?;
+
+        // lets upadate the balances here
+        self.set_balance(&caller, new_caller_balance);
+        self.set_balance(&to, new_to_balance);
+
+        Ok(())
+    }
+}
+
+
+mod tests{
+    use crate::balances::Pallet;
+
+#[test]
+pub fn init_balance() {
+    let mut balances = Pallet::new();
+
+    assert_eq!(balances.balance(&"Alice".to_string()), 0);
+    balances.set_balance(&"Alice".to_string(), 100);
+    assert_eq!(balances.balance(&"Alice".to_string()), 100);
+    assert_eq!(balances.balance(&"Bob".to_string()), 0)
+}
+
+
+#[test]
+pub fn transfer_balance() {
+    let alice = "Alice".to_string();
+    let bob = "Bob".to_string();
+
+    let mut balances = Pallet::new();
+
+    balances.set_balance(&alice.clone(), 100);
+    let _ = balances.transfer(alice.clone(), bob.clone(), 90);
+
+    assert_eq!(balances.balance(&bob), 90);
+    assert_eq!(balances.balance(&alice), 10);
+}
+
+#[test]
+pub fn test_insufficient_balance_case() {
+    let alice = "Alice".to_string();
+    let bob = "Bob".to_string();
+
+    let mut balances = Pallet::new();
+
+    balances.set_balance(&alice, 100);
+    let result = balances.transfer(alice.clone(), bob.clone(), 110);
+
+    assert_eq!(result, Err("Insufficient Balance"));
+    assert_eq!(balances.balance(&alice), 100);
+    assert_eq!(balances.balance(&bob), 0);
+}
+
+#[test]
+pub fn test_overflow_case() {
+    let alice = "Alice".to_string();
+    let bob ="bob".to_string();
+
+    let mut balances = Pallet::new();
+
+    balances.set_balance(&bob, u128::MAX);
+    balances.set_balance(&alice, 100);
+
+    let result = balances.transfer(alice.clone(), bob.clone(), 1);
+    assert_eq!(result, Err("Overflow"));
+    assert_eq!(balances.balance(&bob), u128::MAX);
+    assert_eq!(balances.balance(&alice), 100);
+}
+
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod balances;
+use std::collections::BTreeMap;
+fn main() {
+
+    let mut pallet = balances::Pallet::new();
+
+}
+
