4817e18926fecb4c24317097f117a9e205c29f83
johnnas12 <yohanesmesfin3@gmail.com>
2026-04-20 14:29:38 +0300

    creating out runtime using system and balances

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 mod balances;
 mod system;
-fn main() {
 
-    let mut balance = balances::Pallet::new();
-    let mut system = system::Pallet::new();
+
+pub struct Runtime {
+    system: system::Pallet,
+    balances: balances::Pallet,
+
+}
+
+impl Runtime {
+    fn new() -> Self {
+        Self {
+            system: system::Pallet::new(),
+            balances: balances::Pallet::new(),
+        }
+    }
+}
+
+fn main() {
+    let runtime = Runtime::new();
+    
 
 }
 
