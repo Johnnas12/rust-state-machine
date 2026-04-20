677245b11c67111a6986986fc13a47e22c2d6f33
johnnas12 <yohanesmesfin3@gmail.com>
2026-04-20 13:31:36 +0300

    implementation of nonce and block number incereament using safe math

--- src/system.rs
+++ src/system.rs
@@ -3,7 +3,7 @@ use std::collections::BTreeMap;
 
 pub struct Pallet {
     block_number: u32,
-    nonce: BTreeMap<String, u32>
+    nonce: BTreeMap<String, u128>
 }
 
 impl Pallet {
@@ -18,6 +18,20 @@ impl Pallet {
         self.block_number
     }
 
+    pub fn inc_block_number(&mut self) {
+        // crashed if overflow for purpose
+        self.block_number = self.block_number.checked_add(1).unwrap();
+    } 
+
+    pub fn inc_nonce(&mut self, who: &String) {
+        let nonce = self.nonce.get(who).unwrap_or(&0);
+        self.nonce.insert(who.clone(), nonce + 1);
+    }
+
+    pub fn get_nonce(&mut self, who: &String) -> u128 {
+        *self.nonce.get(who).unwrap_or(&0)
+    }
+
 }
 
 
@@ -25,6 +39,23 @@ mod test {
     #[test]
     fn init_system() {
        let system = super::Pallet::new();
-       assert_eq!(system.block_number(), 0) 
+       assert_eq!(system.block_number(), 0)
+    }
+
+    #[test]
+    fn inc_block_number() {
+        let mut system = super::Pallet::new();
+        system.inc_block_number();
+        assert_eq!(system.block_number(), 1)
+
+    }
+    #[test]
+    fn inc_nonce() {
+        let alice = String::from("alice");
+
+        let mut system = super::Pallet::new();
+        system.inc_nonce(&alice.clone());
+        assert_eq!(system.get_nonce(&alice), 1);
+
     }
 }
