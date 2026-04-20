17efd728fbd400fbc6d4a57771cbd9f1aa2b096b
johnnas12 <yohanesmesfin3@gmail.com>
2026-04-20 14:40:32 +0300

    simulate the transactions

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,33 @@ impl Runtime {
 }
 
 fn main() {
-    let runtime = Runtime::new();
+    // simulate the transactions and all the other things
+
+    let mut runtime = Runtime::new();
+
+    let alice = "alice".to_string();
+    let bob = "bob".to_string();
+    let charlie = "charlie".to_string();
+
+    // initialize alices balance
+    runtime.balances.set_balance(&alice, 100);
+
+    runtime.system.inc_block_number();
+
+    assert_eq!(runtime.system.block_number(), 1);
+
+    runtime.system.inc_nonce(&alice);
+    let _ = runtime.balances.transfer(alice.clone(), bob.clone(), 30)
+            .map_err(|e| println!("Error: {:?}", e));
     
+    let _ = runtime.balances.transfer(alice.clone(), charlie.clone(), 20)
+        .map_err(|e| println!("Error: {:?}", e));
+
+
+
+
+
+
 
 }
 
