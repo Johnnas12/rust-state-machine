619c287733b9666ee2277ba98e7430861061086e
johnnas12 <yohanesmesfin3@gmail.com>
2026-04-21 20:09:38 +0300

    implement and testing the proof of existence pallet

--- src/proof_of_existance.rs
+++ src/proof_of_existance.rs
@@ -1,5 +1,6 @@
 use core::fmt::Debug;
 use std::collections::BTreeMap;
+use crate::support::DispatchResult;
 
 pub trait Config: crate::system::Config {
 	/// The type which represents the content that can be claimed using this pallet.
@@ -25,5 +26,67 @@ impl<T: Config> Pallet<T> {
         Self {
             claims: BTreeMap::new()
         }
+	}
+
+    pub fn get_claims(&self, claim: &T::Content) -> Option<&T::AccountID> {
+        self.claims.get(claim)
+    }
+
+    pub fn create_claim(&mut self, caller: T::AccountID, claim: T::Content) -> DispatchResult {
+        if self.claims.contains_key(&claim){
+            return Err(&"This content is already claimed");
+        }
+
+        self.claims.insert(claim, caller);
+        Ok(())
+    }
+
+    pub fn revoke_claim(&mut self, caller: T::AccountID, claim: T::Content) -> DispatchResult {
+        let claim_owner  = self.get_claims(&claim).ok_or("Claim owner doesnt exist")?;
+        if claim_owner != &caller {
+            return  Err("Caller is not the owner of the claim");
+        }
+        self.claims.remove(&claim);
+		Ok(())
+	}
+
+
+}
+
+
+#[cfg(test)]
+mod test {
+	struct TestConfig;
+
+	impl super::Config for TestConfig {
+		type Content = &'static str;
+	}
+
+	impl crate::system::Config for TestConfig {
+		type AccountID = &'static str;
+		type BlockNumber = u32;
+		type Nonce = u32;
+	}
+
+	#[test]
+	fn basic_proof_of_existence() {
+
+        let mut poe = super::Pallet::<TestConfig>::new();
+        let _ = poe.create_claim("alice", "my_document");
+
+        assert_eq!(poe.get_claims(&"my_document"), Some(&"alice"));
+
+        let res = poe.revoke_claim("bob", "my_document");
+        assert_eq!(res, Err("Caller is not the owner of the claim"));
+
+        let res2 = poe.create_claim("bob", "my_document");
+        assert_eq!(res2, Err("This content is already claimed"));
+
+        let res3 = poe.revoke_claim("alice", "non existant");
+        assert_eq!(res3, Err("Claim owner doesnt exist"));
+
+        let res = poe.revoke_claim("alice", "my_document");
+        assert_eq!(res, Ok(()));
+        assert_eq!(poe.get_claims(&"my_document"), None);
 	}
 }
